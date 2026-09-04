cbeae2b7768b92135b86634319807e971c14169a
Raju <kraju09186@gmail.com>
2026-09-04 17:27:44 +0530

    Typo fixed

--- src/utils/ApiResponse.js
+++ src/utils/ApiResponse.js
@@ -1,8 +1,8 @@
 class ApiResponse {
-    constructor(statuscode, data, message = "Success"){
-        this.statuscode = statuscode
+    constructor(statusCode, data, message = "Success"){
+        this.statusCode = statusCode
         this.data = data
         this.message = message
-        this.success = statuscode < 400
+        this.success = statusCode < 400
     }
 }
